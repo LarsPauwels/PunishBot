c255164ccfbd2c0993dc08912843eb4eafe1561f
larspauwels <lars.pauwels@telenet.be>
2020-10-30 17:51:56 +0100

    adding help command

--- main.js
+++ main.js
@@ -222,7 +222,7 @@ Client.once('ready', () => {
 
 	command(Client, process.env.HELP, message => {
 		console.log('Typed help');
-		commands.HelpCommand(message, 'PunishMeThanos Command Help', [
+		commands.HelpCommand("", 'PunishMeThanos Command Help', [
 			{ name: 'Daddy Command', value: 'Returns appropriate images of our lord and Thanos.' },
 			{ name: 'Pay Command', value: 'Pay with getting your soul crushed or leave.' },
 			{ name: 'Punish Command', value: 'Send a naughty boy to Thanos Dungeon. (Use @username)' }
