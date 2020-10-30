2c0cfce628d0209af2a4f2aabcab9766cd7c3a11
larspauwels <lars.pauwels@telenet.be>
2020-10-30 16:40:50 +0100

    added pay command

--- main.js
+++ main.js
@@ -58,7 +58,7 @@ class Commands {
 			.setColor('#0099ff')
 			.setTitle(text)
 			.setImage(image)
-			.description()
+			.description(desc)
 			.setTimestamp()
 			.setFooter('React with ❌ to delete this post.');
 	}
